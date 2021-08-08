cb84fd695f31283720ef11a827ccfdcea291914a
Phil <phil.dev@mailbox.org>
2021-08-08 17:01:41 +0200

    added configmap and DaemonSet for fluentd

--- k8s/create_cluster.sh
+++ k8s/create_cluster.sh
@@ -1,8 +1,13 @@
-k3d cluster create mycluster \
+#!/bin/sh
+
+CLUSTERNAME=mycluster
+
+k3d cluster create "${CLUSTERNAME}" \
 --api-port 127.0.0.1:6550 \
 -p 80:80@loadbalancer \
 -p 443:443@loadbalancer \
 -p 3000:3000@loadbalancer \
 -p 9090:9090@loadbalancer \
 --k3s-server-arg "--no-deploy=traefik" \
+--k3s-server-arg "--flannel-backend=vxlan" \
 --agents 2 --servers 1
